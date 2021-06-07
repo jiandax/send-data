8bcac0418628dc8b4ab064273bea5cd8d84ff55a
jiandax-pc <jiandax@163.com>
2021-06-07 21:20:34 +0800

    .

---
+++ cms-start.bat
@@ -0,0 +1,10 @@
+@echo off
+cd/
+e:
+cd DevProjects\furesky-jar
+
+echo -----------------------------------------------------------------
+echo     进入目录：%~dp0
+echo     运行命令：java -jar furesky-1.0.0-SNAPSHOT.jar
+echo -----------------------------------------------------------------
+java -jar furesky-1.0.0-SNAPSHOT.jar
